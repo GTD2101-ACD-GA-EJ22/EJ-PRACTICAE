570d193b703ebf65a16f7526e1222ddeac38a97a
MarianaArzapalo <19170713@itculiacan.edu.mx>
2022-02-16 17:29:46 +0000

    nombre

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+
+    nombre=input("Dame tu nombre")
+    apellido_p=input("Dame tu apellido paterno")
+apellido_m=input("Dame tu apellido materno")
+
+nombre_completo=nombre  +""+apellido_p  +""+apellido_m 
+
+print(nombre_completo)
+
+pass
 
 if __name__=='__main__':
     main()
+med
